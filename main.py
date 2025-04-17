27c6f39ec927a863c77e9230c33227e81658a0d7
Tryny8 <vitry.pro.a@gmail.com>
2025-04-17 16:00:31 +0200

    B: Refactor

--- main.py
+++ main.py
@@ -3,8 +3,24 @@ def get_book_text(path_file: str) -> str:
         file_contents = f.read()
     return file_contents
 
+def count_word(text: str) -> int:
+    list_word = []
+    list_word_raw = text.split(" ")
+    for word in list_word_raw:
+        if word != '':
+            if '\n' in word:
+                temp = word.split("\n")
+                for element in temp:
+                    if element != '':
+                        list_word.append(element)
+            else:
+                list_word.append(word)
+    count = len(list_word)
+    return count
+
 def main():
     file = get_book_text("books/frankenstein.txt")
-    print(file)
+    num_words = count_word(file)
+    print(f"{num_words} words found in the document")
 
 main()
