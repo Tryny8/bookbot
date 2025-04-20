89f3e93d8fc3366250fe91bf48a45c164d68b0a8
Tryny8 <vitry.pro.a@gmail.com>
2025-04-20 05:09:34 +0200

    E: Print a Report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_num_caract
+from stats import get_num_words, get_num_caract, sort_dict
 
 
 def get_book_text(path_file: str) -> str:
@@ -6,11 +6,24 @@ def get_book_text(path_file: str) -> str:
         file_contents = f.read()
     return file_contents
 
+def print_report(file, count_word, list_caract):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file}...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_word} total words")
+    print("--------- Character Count -------")
+    for dico in list_caract:
+        if dico["caract"].isalpha():
+            print(f"{dico["caract"]}: {dico["num"]}")
+    print("============= END ===============")
+    
+
 def main():
-    file = get_book_text("books/frankenstein.txt")
-    num_words = get_num_words(file)
-    print(f"{num_words} words found in the document")
-    num_caract = get_num_caract(file)
-    print(num_caract)
+    path_file = "books/frankenstein.txt"
+    str_file = get_book_text(path_file)
+    num_words = get_num_words(str_file)
+    num_caract = get_num_caract(str_file)
+    sorted_caract = sort_dict(num_caract)
+    print_report(path_file, num_words, sorted_caract)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,5 @@
+from operator import itemgetter
+
 def get_num_words(text: str) -> int:
     list_word = []
     list_word_raw = text.split(" ")
@@ -13,7 +15,7 @@ def get_num_words(text: str) -> int:
     count = len(list_word)
     return count
 
-def get_num_caract(text_raw:str) -> dict:
+def get_num_caract(text_raw: str) -> dict:
     caract_dict = {}
     text: str = text_raw.lower()
     for caract in text:
@@ -22,3 +24,14 @@ def get_num_caract(text_raw:str) -> dict:
         else:
             caract_dict[caract] = 1
     return caract_dict
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_dict(caract_raw_dict: dict) -> list[dict]:
+    tmp_caract_list: list[dict] = []
+    sorted_caract_list: list[dict] = []
+    for key, value in caract_raw_dict.items():
+        tmp_caract_list.append({"caract": key, "num": value})
+    sorted_caract_list = sorted(tmp_caract_list, key=itemgetter('num'), reverse=True)
+    return sorted_caract_list
